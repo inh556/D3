a6473803ea704ce007db0e3086d516490550fd09
inh556 <yanbin.jin@gmail.com>
2019-02-08 10:14:06 -0800

    made the first barchart

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
-const body = d3.select('body');
-body.append('h1').text('hello, D3')
-const h1 = body.select('h1');
+const dataset = [8, 48, 13, 23,30];
 
-h1.attr('id', 'somethingelse')
-  .attr('class','myclass')
-  .classed('another-class', 'true')
-  .classed('myclass', false)
+svg = d3.select('body')
+    .append('svg')
+    .attr('width', 600)
+    .attr('height', 400)
 
- 
+svg.selectAll('rect')
+    .data(dataset)
+    .enter()
+    .append('rect')
+    .attr('x', (d, i) => {return i * 101})
+    .attr('y', (d, i) => {return 400 - d * 10})
+    .attr('width', 100)
+    .attr('height', (d) => {return d * 10})
+    .attr('fill', 'orange')
