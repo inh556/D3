8ec07b4fb604ea4572b39bc99eeb094f2113ce5a
inh556 <yanbin.jin@gmail.com>
2019-02-07 21:52:10 -0800

    barchart

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
-const dataset = [5, 10, 5, 20, 25, 30]
+const dataset = [5, 10, 15, 20, 25, 30]
 
-const el = d3.select('body')
-              .selectAll('p')
+// const el = d3.select('body')
+//               .selectAll('p')
+//               .data(dataset)
+//               .enter()
+//               .append('p')
+//               .text('Hello body')
+
+const el = d3.select('#barchart')
+              .selectAll('div')
               .data(dataset)
               .enter()
-              .append('p')
-              .text('Hello body')
-
+              .append('div')
+              .attr('class', 'bar')
+              .style('height', function(x) {
+                return x + 'px';
+              })
